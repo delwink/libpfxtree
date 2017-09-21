886ee4562ed98f97320b30a1adb4e5b48a2b42a5
David McMackins II <contact@mcmackins.org>
2017-09-21 08:23:44 -0500

    Change license to COIL 0.5

--- pfxtree-test.c
+++ pfxtree-test.c
@@ -2,17 +2,38 @@
  *  pfxtree-test - Delwink prefix tree library unit test
  *  Copyright (C) 2017 Delwink, LLC
  *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU Affero General Public License as published by
- *  the Free Software Foundation, version 3 only.
+ * Redistributions, modified or unmodified, in whole or in part, must retain
+ * applicable copyright or other legal privilege notices, these conditions, and
+ * the following license terms and disclaimer.  Subject to these conditions,
+ * the holder(s) of copyright or other legal privileges, author(s) or
+ * assembler(s), and contributors of this work hereby grant to any person who
+ * obtains a copy of this work in any form:
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Affero General Public License for more details.
+ * 1. Permission to reproduce, modify, distribute, publish, sell, sublicense,
+ * use, and/or otherwise deal in the licensed material without restriction.
  *
- *  You should have received a copy of the GNU Affero General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 2. A perpetual, worldwide, non-exclusive, royalty-free, irrevocable patent
+ * license to reproduce, modify, distribute, publish, sell, use, and/or
+ * otherwise deal in the licensed material without restriction, for any and all
+ * patents:
+ *
+ *     a. Held by each such holder of copyright or other legal privilege,
+ *     author or assembler, or contributor, necessarily infringed by the
+ *     contributions alone or by combination with the work, of that privilege
+ *     holder, author or assembler, or contributor.
+ *
+ *     b. Necessarily infringed by the work at the time that holder of
+ *     copyright or other privilege, author or assembler, or contributor made
+ *     any contribution to the work.
+ *
+ * NO WARRANTY OF ANY KIND IS IMPLIED BY, OR SHOULD BE INFERRED FROM, THIS
+ * LICENSE OR THE ACT OF DISTRIBUTION UNDER THE TERMS OF THIS LICENSE,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR
+ * A PARTICULAR PURPOSE, AND NONINFRINGEMENT.  IN NO EVENT SHALL THE AUTHORS,
+ * ASSEMBLERS, OR HOLDERS OF COPYRIGHT OR OTHER LEGAL PRIVILEGE BE LIABLE FOR
+ * ANY CLAIM, DAMAGES, OR OTHER LIABILITY, WHETHER IN ACTION OF CONTRACT, TORT,
+ * OR OTHERWISE ARISING FROM, OUT OF, OR IN CONNECTION WITH THE WORK OR THE USE
+ * OF OR OTHER DEALINGS IN THE WORK.
  */
 
 #include <assert.h>
