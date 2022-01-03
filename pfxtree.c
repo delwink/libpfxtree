33cd809984b496bf3e55d2b789c472471282e6c5
David McMackins II <contact@mcmackins.org>
2022-01-03 07:22:46 -0600

    Change license to COIL 1.0

--- pfxtree.c
+++ pfxtree.c
@@ -2,38 +2,31 @@
  *  libpfxtree - Delwink prefix tree library
  *  Copyright (C) 2015, 2017, 2020 Delwink, LLC
  *
- * Redistributions, modified or unmodified, in whole or in part, must retain
- * applicable copyright or other legal privilege notices, these conditions, and
- * the following license terms and disclaimer.  Subject to these conditions,
- * the holder(s) of copyright or other legal privileges, author(s) or
- * assembler(s), and contributors of this work hereby grant to any person who
- * obtains a copy of this work in any form:
+ *  Redistributions, modified or unmodified, in whole or in part, must retain
+ *  applicable notices of copyright or other legal privilege, these conditions,
+ *  and the following license terms and disclaimer.  Subject to these
+ *  conditions, each holder of copyright or other legal privileges, author or
+ *  assembler, and contributor of this work, henceforth "licensor", hereby
+ *  grants to any person who obtains a copy of this work in any form:
  *
- * 1. Permission to reproduce, modify, distribute, publish, sell, sublicense,
- * use, and/or otherwise deal in the licensed material without restriction.
+ *  1. Permission to reproduce, modify, distribute, publish, sell, sublicense,
+ *  use, and/or otherwise deal in the licensed material without restriction.
  *
- * 2. A perpetual, worldwide, non-exclusive, royalty-free, irrevocable patent
- * license to reproduce, modify, distribute, publish, sell, use, and/or
- * otherwise deal in the licensed material without restriction, for any and all
- * patents:
+ *  2. A perpetual, worldwide, non-exclusive, royalty-free, gratis, irrevocable
+ *  patent license to make, have made, provide, transfer, import, use, and/or
+ *  otherwise deal in the licensed material without restriction, for any and
+ *  all patents held by such licensor and necessarily infringed by the form of
+ *  the work upon distribution of that licensor's contribution to the work
+ *  under the terms of this license.
  *
- *     a. Held by each such holder of copyright or other legal privilege,
- *     author or assembler, or contributor, necessarily infringed by the
- *     contributions alone or by combination with the work, of that privilege
- *     holder, author or assembler, or contributor.
- *
- *     b. Necessarily infringed by the work at the time that holder of
- *     copyright or other privilege, author or assembler, or contributor made
- *     any contribution to the work.
- *
- * NO WARRANTY OF ANY KIND IS IMPLIED BY, OR SHOULD BE INFERRED FROM, THIS
- * LICENSE OR THE ACT OF DISTRIBUTION UNDER THE TERMS OF THIS LICENSE,
- * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR
- * A PARTICULAR PURPOSE, AND NONINFRINGEMENT.  IN NO EVENT SHALL THE AUTHORS,
- * ASSEMBLERS, OR HOLDERS OF COPYRIGHT OR OTHER LEGAL PRIVILEGE BE LIABLE FOR
- * ANY CLAIM, DAMAGES, OR OTHER LIABILITY, WHETHER IN ACTION OF CONTRACT, TORT,
- * OR OTHERWISE ARISING FROM, OUT OF, OR IN CONNECTION WITH THE WORK OR THE USE
- * OF OR OTHER DEALINGS IN THE WORK.
+ *  NO WARRANTY OF ANY KIND IS IMPLIED BY, OR SHOULD BE INFERRED FROM, THIS
+ *  LICENSE OR THE ACT OF DISTRIBUTION UNDER THE TERMS OF THIS LICENSE,
+ *  INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR
+ *  A PARTICULAR PURPOSE, AND NONINFRINGEMENT.  IN NO EVENT SHALL THE AUTHORS,
+ *  ASSEMBLERS, OR HOLDERS OF COPYRIGHT OR OTHER LEGAL PRIVILEGE BE LIABLE FOR
+ *  ANY CLAIM, DAMAGES, OR OTHER LIABILITY, WHETHER IN ACTION OF CONTRACT,
+ *  TORT, OR OTHERWISE ARISING FROM, OUT OF, OR IN CONNECTION WITH THE WORK OR
+ *  THE USE OF OR OTHER DEALINGS IN THE WORK.
  */
 
 #include <stdlib.h>
